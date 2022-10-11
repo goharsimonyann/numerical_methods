4f3c19c45787e4785b2cb3060a582f194744c916
Gohar Simonyan <82412056+goharsimonyann@users.noreply.github.com>
2022-10-11 09:46:53 +0400

    updated main.js

--- main.js
+++ main.js
@@ -36,3 +36,30 @@ function main(x = -4, xnext = g(x), eps){
     
     return xnext;
 }
+
+
+// metod Newtona
+
+function f(x){
+    return Math.pow(x, 5) - 5 * x - 22
+}
+
+function f_(x){
+    return 5 * Math.pow(x, 4) - 5;
+}
+
+function xn(x){
+    return x - (f(x) / f_(x));
+}
+
+function main(x, eps, count){
+    let xnext = xn(x);
+    
+    while(Math.abs(xnext - x) > eps){
+        x = xnext;
+        xnext = xn(x);
+        count++;
+    }
+    
+    return xnext + " steps " + count;
+}
